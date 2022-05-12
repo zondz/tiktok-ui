a8a2fe79e5b9aa13638a02e1a29a64cb489edbdf
= <yen17032000@gmail.com>
2022-05-12 15:51:06 +0700

    dung khung layout mac dinh

--- src/components/Layout/DefaultLayout/SideBar/index.js
+++ src/components/Layout/DefaultLayout/SideBar/index.js
@@ -1,5 +1,13 @@
+import styles from './SideBar.module.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
 function SideBar() {
-  return <h2>SideBar</h2>;
+  return (
+    <aside className={cx('wrapper')}>
+      <h2>Side bar</h2>
+    </aside>
+  );
 }
 
 export default SideBar;
